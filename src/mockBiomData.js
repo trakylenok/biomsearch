65d6218eecf76bf9d60f90187b652aed6d738a9f
AnastasiaBogacheva <trakylenok@gmail.com>
2024-04-24 21:04:09 +0300

    BS-2 Add tests

---
+++ src/mockBiomData.js
@@ -0,0 +1,7 @@
+export const mockBiomData = {
+  name: ["name1", "name2"],
+  taxId: ["tax1", "tax2"],
+  abundanceScore: [0.5, 0.6],
+  relativeAbundance: [0.005, 0.006],
+  uniqueMatchesFrequency: [10, 20],
+};
